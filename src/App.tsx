0006814d941719b75545af2ad77e4a9f02069890
SouzaVitoria <viihsouza161@gmail.com>
2022-08-02 13:59:17 -0300

    added global styles with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react"
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button/Button"
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Button variant="primary" />
       <Button variant="secondary" />
     </ThemeProvider>
