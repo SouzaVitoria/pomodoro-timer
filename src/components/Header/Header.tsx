576044c9b3542aaf664a27f984ab14df304a2f78
SouzaVitoria <viihsouza161@gmail.com>
2022-08-03 15:06:14 -0300

    added Header component and its stylizations

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,22 @@
+import { NavLink } from "react-router-dom";
+import { Scroll, Timer } from "phosphor-react";
+import { HeaderContainer } from "./styles";
+import logoIgnite from "../../assets/logo-ignite.svg";
+
 export function Header() {
-  return <h1>Header</h1>;
+  return (
+    <HeaderContainer>
+      <span>
+        <img src={logoIgnite} alt="" />
+      </span>
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
 }
