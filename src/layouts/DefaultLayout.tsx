1956928a1a3aaf35319fd5937b67c5222dac359c
SouzaVitoria <viihsouza161@gmail.com>
2022-08-03 14:46:21 -0300

    added default Layout styles

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header/Header";
+import { LayoutContainer } from "../styles/LayoutContainer";
 
 export function DefaultLayout() {
   return (
-    <>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </>
+    </LayoutContainer>
   );
 }
